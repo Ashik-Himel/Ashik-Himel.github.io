4ba6ded16d4d88e548c0cf65e5d87cd9cfd6759d
Ashik-Himel <ashikujjamanhimel@gmail.com>
2021-11-05 08:36:24 +0600

    hero section done

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelectorAll('.nav-link');
+const navSection = document.querySelector('.nav-section');
+const navScrollHeight = navSection.offsetHeight;
+
+function navAndCrossToggle() {
+    navSection.classList.toggle('show-nav');
+    hamburger.classList.toggle('cross-hamburger');
+}
+function navAndCrossClose() {
+    navSection.classList.remove('show-nav');
+    hamburger.classList.remove('cross-hamburger');
+}
+
+hamburger.onclick = navAndCrossToggle;
+navLink.forEach(n => n.addEventListener('click', navAndCrossClose));
+
+window.addEventListener('click' , function(e) {
+    if(navSection.contains(e.target)) {
+        
+    }
+    else{
+        return navAndCrossClose();
+    }
+});
+
+window.addEventListener('scroll' , function() {
+    const scrollLength = window.scrollY;
+    if (scrollLength >= navScrollHeight) {
+        navSection.classList.add('nav-sticky');
+    }
+    else {
+        navSection.classList.remove('nav-sticky');
+    }
+    console.log(scrollLength);
+});
