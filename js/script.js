fa6223211827c1c9dc1be42e3dc0dde01b8a35c9
Ashik-Himel <ashikujjamanhimel@gmail.com>
2021-11-06 08:02:38 +0600

    scroll down added on mobile

--- js/script.js
+++ js/script.js
@@ -34,3 +34,22 @@ window.addEventListener('scroll' , function() {
     }
     console.log(scrollLength);
 });
+const swiper = new Swiper('.swiper', {
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
