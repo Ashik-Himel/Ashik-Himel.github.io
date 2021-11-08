ae4c0100fb60fe90523bc53e0fb236cc707a2ce5
Ashik-Himel <ashikujjamanhimel@gmail.com>
2021-11-08 07:56:13 +0600

    Portfolio Section Done!

--- script.js
+++ script.js
@@ -85,6 +85,7 @@ const swiper = new Swiper('.swiper', {
     },
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
     },
     navigation: {
       nextEl: '.swiper-button-next',
